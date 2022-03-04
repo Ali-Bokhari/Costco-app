c2a3f660a1c67125de062074fbdb0549a96798c2
Syed Ali Bokhari <syebokha@publicisgroupe.net>
2022-03-04 18:20:49 -0500

    Minor fix

--- src/Category.js
+++ src/Category.js
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Accordion from 'react-bootstrap/Accordion';
-import Form from 'react-bootstrap/Form';
 
 import './Category.css'
 
