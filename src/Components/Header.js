d54f6934105f39045db686c1131c75f5a6592785
MaxSwen <pls6666@126.com>
2022-03-08 17:50:05 -0500

    preliminary map page components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import "./Header.css"
 const Header = (props)=>{
     return(
         <div className="headerdiv">
-            <br /><h1 className="my-3 py-2 text-center header">{props.title}</h1>
+            <br /><h1 className="py-2 text-center header">{props.title}</h1>
         </div>
     )
 }
--- src/Pages/Map.js
+++ src/Pages/Map.js
@@ -1,11 +1,48 @@
 import MapArea from '../Components/Map/MapArea';
 import BottomNav from "../Components/BottomNav";
 import Header from "../Components/Header";
+import {Card} from "react-bootstrap/esm/index";
+import "./Map.css"
+import {Row, Col, Button} from "react-bootstrap";
+import {faCircleCheck, faLocationArrow} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function Map (props){
     return (
-        <div>
+        <div className="map-page">
             <Header title={"Navigation - Map"}/>
+            <Card className="store-info-card">
+                <Card.Body>
+                    <h6><strong>My Store</strong></h6>
+                    <Row className="store-info">
+                        <Col xs={9}>
+                            <strong>Toronto - 100 Billy Bishop Way</strong>
+                            <div>Closes at 10pm</div>
+                        </Col>
+                        <Col xs={3} id="change-store-btn">Change</Col>
+                    </Row>
+                </Card.Body>
+            </Card>
+
+            <div className="overview-card">
+                <Card>
+                    <Card.Body>
+                        <div className="overview-info">
+                            <div>
+                                <strong>Get it done</strong>
+                                <div id="todo-count">6 items to complete</div>
+                            </div>
+                            <div id="navigation-btn">
+                                <Button variant="primary">
+                                    <FontAwesomeIcon className="nav-icon" icon={faLocationArrow} />
+                                    Start Navigation
+                                </Button>
+                            </div>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </div>
+
             <MapArea slist={props.slist}/>
             <BottomNav/>
         </div>
