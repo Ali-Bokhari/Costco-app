d00151528e6e1ecffe0f4be37fd3a502146e8b9f
Syed Ali Bokhari <syebokha@publicisgroupe.net>
2022-03-08 14:00:31 -0500

    Marker library working

--- src/Components/Map/MapArea.js
+++ src/Components/Map/MapArea.js
@@ -4,23 +4,14 @@ import { useState } from 'react';
 import costcoMap from '../../images/LabledMap.png'
 
 function MapArea(props) {
-    // const [markers, setMarkers] =
-    // useState <
-    // Array <
-    // Marker >>
-    //     [
-    //         {
-    //             top: 10, //10% of the image relative size from top
-    //             left: 50, //50% of the image relative size from left
-    //         },
-    //     ];
-    // return(
-    //     <ImageMarker
-    // src={costcoMap}
-    // markers={markers}
-    // onAddMarker={(marker:Marker) => setMarkers([...markers, marker])}
-    // />
-    // );
+    let [markers, setMarkers] = useState([]);
+    return(
+        <ImageMarker
+        src={costcoMap}
+        markers={markers}
+        onAddMarker={(marker) => setMarkers((prev) => [...prev, marker])}
+      />
+    );
 }
 
 export default MapArea;
