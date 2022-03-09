76aae0225d6546c99dd83f585e3a83b91f58448d
Syed Ali Bokhari <syebokha@publicisgroupe.net>
2022-03-09 12:11:59 -0500

    Adding blue marker

--- src/Components/Map/MapArea.js
+++ src/Components/Map/MapArea.js
@@ -3,26 +3,45 @@ import { useState} from 'react';
 
 import costcoMap from '../../images/LabledMap.png'
 import redMarker from '../../images/redmarker.png'
+import blueMarker from '../../images/bluemarker.png'
 
 import Image from 'react-bootstrap/Image';
 import {coordinates_arr} from "./aisle-coordinates"
 
+let markcolor = true;
+
 function MapArea(props) {
     let s_markers=[];
+
     for (const i of props.slist){
         s_markers.push(coordinates_arr[i.aisle-1]);
     }
     const [markers, setMarkers] = useState(s_markers);
 
     function CustomMarker() {
+      if(markcolor) {
         return (
           <Image src={redMarker}/>
         );
-      };
+      } else {
+        return (
+          <Image src={blueMarker}/>
+        );
+      }
+        
+    }
+
+    function addMarker (marker) {
+      markcolor = false;
+      setMarkers((prev) => [...prev, marker]);
+    }
+
     return(
-        <ImageMarker src={costcoMap} markers={markers} onAddMarker={(marker) => setMarkers((prev) => [...prev, marker])} markerComponent={CustomMarker}/>
+        <ImageMarker src={costcoMap} markers={markers} onAddMarker={addMarker} markerComponent={CustomMarker}/>
     );
 }
+
+// (marker) => setMarkers((prev) => [...prev, marker])
 
 export default MapArea;
 
