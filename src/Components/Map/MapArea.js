dac300b58912c32bf85bdc03c788b50ea08c855b
MaxSwen <pls6666@126.com>
2022-03-09 12:16:24 -0500

    added pins images

--- src/Components/Map/MapArea.js
+++ src/Components/Map/MapArea.js
@@ -1,5 +1,5 @@
 import ImageMarker from 'react-image-marker';
-import { useState} from 'react';
+import {useState} from 'react';
 
 import costcoMap from '../../images/LabledMap.png'
 import redMarker from '../../images/redmarker.png'
@@ -8,19 +8,20 @@ import Image from 'react-bootstrap/Image';
 import {coordinates_arr} from "./aisle-coordinates"
 
 function MapArea(props) {
-    let s_markers=[];
-    for (const i of props.slist){
-        s_markers.push(coordinates_arr[i.aisle-1]);
+    let s_markers = [];
+    for (const i of props.slist) {
+        s_markers.push(coordinates_arr[i.aisle - 1]);
     }
     const [markers, setMarkers] = useState(s_markers);
 
     function CustomMarker() {
         return (
-          <Image src={redMarker}/>
+            <Image src={redMarker}/>
         );
-      };
-    return(
-        <ImageMarker src={costcoMap} markers={markers} onAddMarker={(marker) => setMarkers((prev) => [...prev, marker])} markerComponent={CustomMarker}/>
+    };
+    return (
+        <ImageMarker src={costcoMap} markers={markers} onAddMarker={(marker) => setMarkers((prev) => [...prev, marker])}
+                     markerComponent={CustomMarker}/>
     );
 }
 
