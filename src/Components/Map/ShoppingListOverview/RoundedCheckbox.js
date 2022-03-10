60e6aa6473f999d345fac7ae3fa2fa56e0573821
Syed Ali Bokhari <syebokha@publicisgroupe.net>
2022-03-10 16:31:41 -0500

    Fixed checkbox toggles

--- src/Components/Map/ShoppingListOverview/RoundedCheckbox.js
+++ src/Components/Map/ShoppingListOverview/RoundedCheckbox.js
@@ -3,7 +3,7 @@ import "./RoundedCheckbox.css"
 const RoundedCheckbox = (props) => {
     return(
         <label className="rounded-checkbox-container">
-            <input type="checkbox" checked={props.isChecked}/>
+            <input type="checkbox" defaultChecked={props.isChecked}/>
             <span className="checkmark"></span>
         </label>
     )
